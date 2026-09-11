70843446c45c50de0208da3279529cbf9fee2898
solfaron <igruk4459@gmail.com>
2026-09-12 00:22:18 +0300

    Deleted garbage code.

--- CLI-Task-Manager/CLI-Task-Manager/Command.cs
+++ CLI-Task-Manager/CLI-Task-Manager/Command.cs
@@ -5,25 +5,4 @@ public class Command
     public string CommandName { get;  set; }
     public int Id { get;  set; }
     public string Text { get;  set; }
-    
-    public Command()
-    {
-    }
-
-    public Command(string commandName, string text)
-    {
-        CommandName = commandName;
-        Text = text;
-    }
-    public Command(string commandName, int id)
-    {
-        CommandName = commandName;
-    }
-    public Command(string commandName, int id, string text)
-    {
-        CommandName = commandName;
-        Id = id;
-        Text = text;
-    }
-   
 }
